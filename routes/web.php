8567694d0847a4010e61893ed49413b588cb5283
abdur <abdur@gmail.com>
2026-01-29 17:51:50 +0600

    Admin Login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/admin/dashboard', [AdminDashboardController::class,'dashboard'])->name('admin.dashboard')->middleware('auth');
