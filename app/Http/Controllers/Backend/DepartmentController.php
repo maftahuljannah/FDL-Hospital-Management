f49e26db1157b2b3a603e0b6cff9faa46dcb388f
maftahuljannah <maftahuljannah@gmail.com>
2026-02-19 10:38:39 +0600

    Resolved DepartmentController merge conflict

--- app/Http/Controllers/Backend/DepartmentController.php
+++ app/Http/Controllers/Backend/DepartmentController.php
@@ -2,33 +2,63 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
 use App\Models\Department;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DepartmentController extends Controller
 {
-    function showDepartment($id = null){
-        $editedDepartment = $id ?  Department::find($id) : null;
-        $departments = Department::latest()->simplePaginate(2);
-       return view('backend.admin.department.index', compact('departments','editedDepartment'));
+    function showDepartment($id = null)
+    {
+        $editedDepartment = $id ? Department::find($id) : null;
+        $departments = Department::latest()->paginate(15);
+
+        return view('backend.admin.department.index', 
+            compact('departments', 'editedDepartment')
+        );
     }
 
-
-    function store(Request $request, $id=null){
+    function store(Request $request, $id = null)
+    {
         $request->validate([
             'title' => 'required',
-            'description' => 'nullable|max:60'
+            'description' => 'nullable|max:50',
         ]);
 
-        $department = Department::updateOrCreate([
-            'id' => $id
-        ],$request->all());
-        $msg = $id ? 'Department has been updated!' : 'Department has been added!';
-        //* Success
-        return back()->with('msg', [
+        Department::updateOrCreate(
+            ['id' => $id],
+            $request->only(['title','description'])
+        );
+
+        $msg = $id 
+            ? 'Department has been Updated Successfully!' 
+            : 'Department Added Successfully!';
+
+        return redirect()->route('admin.department')->with('msg', [
             'type' => 'success',
             'content' => $msg
+        ]);
+    }
+
+    function deleteDepartment($id)
+    {
+        Department::findOrFail($id)->delete();
+
+        return back()->with('msg', [
+            'type' => 'success',
+            'content' => 'Department has been Deleted Successfully'
+        ]);
+    }
+
+    public function status($id)
+    {
+        $department = Department::findOrFail($id);
+        $department->status = !$department->status;
+        $department->save();
+
+        return back()->with('msg', [
+            'type' => 'success',
+            'content' => 'Department status updated successfully!'
         ]);
     }
 }
