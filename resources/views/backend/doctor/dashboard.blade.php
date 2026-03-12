0d06f63c4cb4c5a4733e77a3122e33fc269d9d1a
mahmudulabedinctg <mahmudulabedinctg@gmail.com>
2026-03-12 11:22:31 +0600

    Initialize doctor routes and views for dashboard, appointments, schedule, patients, and leave

---
+++ resources/views/backend/doctor/dashboard.blade.php
@@ -0,0 +1,245 @@
+@extends('backend.doctor.layout')
+@section('body')
+    <!-- Content -->
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="row">
+            <div class="col-lg-8 mb-4 order-0">
+                <div class="card">
+                    <div class="d-flex align-items-end row">
+                        <div class="col-sm-7">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Congratulations Doctor!</h5>
+                                <p class="mb-4">
+                                    You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
+                                    your profile.
+                                </p>
+
+                                <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
+                            </div>
+                        </div>
+                        <div class="col-sm-5 text-center text-sm-left">
+                            <div class="card-body pb-0 px-0 px-md-4">
+                                <img src="{{ asset('backend/assets/img/illustrations/man-with-laptop-light.png') }}"
+                                    height="140" alt="View Badge User"
+                                    data-app-dark-img="illustrations/man-with-laptop-dark.png') }}"
+                                    data-app-light-img="illustrations/man-with-laptop-light.png') }}" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 order-1" style="height: fit-content;">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 col-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="card-title d-flex align-items-start justify-content-between">
+                                    <div class="avatar flex-shrink-0">
+                                        <i class="bx bx-group" style="font-size: xx-large;"></i>
+                                    </div>
+                                    <div class="dropdown">
+                                        <button class="btn p-0" type="button" id="cardOpt3" data-bs-toggle="dropdown"
+                                            aria-haspopup="true" aria-expanded="false">
+                                            <i class="bx bx-dots-vertical-rounded"></i>
+                                        </button>
+                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
+                                            <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                                            <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span class="fw-semibold d-block mb-1">Total Patient</span>
+                                <h3 class="card-title mb-2">2628</h3>
+                                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +72.80%</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 col-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="card-title d-flex align-items-start justify-content-between">
+                                    <div class="avatar flex-shrink-0">
+                                        <img src="{{ asset('backend/assets/img/icons/unicons/chart-success.png') }}"
+                                            alt="Credit Card" class="rounded" />
+                                    </div>
+                                    <div class="dropdown">
+                                        <button class="btn p-0" type="button" id="cardOpt6" data-bs-toggle="dropdown"
+                                            aria-haspopup="true" aria-expanded="false">
+                                            <i class="bx bx-dots-vertical-rounded"></i>
+                                        </button>
+                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt6">
+                                            <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                                            <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span>Follow Ups</span>
+                                <h3 class="card-title text-nowrap mb-1">679</h3>
+                                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.42%</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-8 order-2 order-md-2">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="card-title d-flex align-items-start justify-content-between">
+                                    <div class="avatar flex-shrink-0">
+                                        <i class="menu-icon tf-icons bx bx-calendar-check" style="font-size: xx-large;"></i>
+                                    </div>
+                                    <div class="d-flex align-items-center gap-2">
+                                        <div class="dropdown">
+                                            <button class="btn btn-sm btn-outline-primary dropdown-toggle" type="button"
+                                                id="growthReportId" data-bs-toggle="dropdown" aria-haspopup="true"
+                                                aria-expanded="false">
+                                                Daily
+                                            </button>
+                                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="growthReportId"
+                                                style="">
+                                                <a class="dropdown-item" href="javascript:void(0);">Weekly</a>
+                                                <a class="dropdown-item" href="javascript:void(0);">Monthly</a>
+                                                <a class="dropdown-item" href="javascript:void(0);">Yearly</a>
+                                                <a class="dropdown-item" href="javascript:void(0);">All Time</a>
+                                            </div>
+                                        </div>
+                                        <div class="dropdown">
+                                            <button class="btn p-0" type="button" id="cardOpt4" data-bs-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">
+                                                <i class="bx bx-dots-vertical-rounded"></i>
+                                            </button>
+                                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt4">
+                                                <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span class="d-block mb-1">Total Appointments</span>
+                                <h3 class="card-title text-nowrap mb-2">456</h3>
+                                <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="card-title d-flex align-items-start justify-content-between">
+                                    <div class="avatar flex-shrink-0">
+                                        <i class="menu-icon tf-icons bx bx-calendar-x" style="font-size: xx-large;"></i>
+                                    </div>
+                                    <div class="d-flex align-items-center gap-2">
+                                        <div class="dropdown">
+                                            <button class="btn btn-sm btn-outline-primary dropdown-toggle" type="button"
+                                                id="growthReportId" data-bs-toggle="dropdown" aria-haspopup="true"
+                                                aria-expanded="false">
+                                                Daily
+                                            </button>
+                                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="growthReportId"
+                                                style="">
+                                                <a class="dropdown-item" href="javascript:void(0);">Weekly</a>
+                                                <a class="dropdown-item" href="javascript:void(0);">Monthly</a>
+                                                <a class="dropdown-item" href="javascript:void(0);">Yearly</a>
+                                                <a class="dropdown-item" href="javascript:void(0);">All Time</a>
+                                            </div>
+                                        </div>
+                                        <div class="dropdown">
+                                            <button class="btn p-0" type="button" id="cardOpt4" data-bs-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">
+                                                <i class="bx bx-dots-vertical-rounded"></i>
+                                            </button>
+                                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt4">
+                                                <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span class="d-block mb-1">Cancel Appointments</span>
+                                <h3 class="card-title text-nowrap mb-2">35</h3>
+                                <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="card-title d-flex align-items-start justify-content-between">
+                                    <div class="avatar flex-shrink-0">
+                                        <i class="menu-icon tf-icons bx bx-calendar-plus" style="font-size: xx-large;"></i>
+                                    </div>
+                                    <div class="dropdown">
+                                        <button class="btn p-0" type="button" id="cardOpt4" data-bs-toggle="dropdown"
+                                            aria-haspopup="true" aria-expanded="false">
+                                            <i class="bx bx-dots-vertical-rounded"></i>
+                                        </button>
+                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt4">
+                                            <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                                            <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span class="d-block mb-1">Rescheduled</span>
+                                <h3 class="card-title text-nowrap mb-2">25</h3>
+                                <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="card-title d-flex align-items-start justify-content-between">
+                                    <div class="avatar flex-shrink-0">
+                                        <i class="menu-icon tf-icons bx bx-calendar-star" style="font-size: xx-large;"></i>
+                                    </div>
+                                    <div class="d-flex align-items-center gap-2">
+                                        <div class="dropdown">
+                                            <button class="btn p-0" type="button" id="cardOpt4" data-bs-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">
+                                                <i class="bx bx-dots-vertical-rounded"></i>
+                                            </button>
+                                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt4">
+                                                <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span class="d-block mb-1">Pre Visit Booking</span>
+                                <h3 class="card-title text-nowrap mb-2">456</h3>
+                                <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-9 col-md-12 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="card-title d-flex align-items-start justify-content-between">
+                                    <div class="avatar flex-shrink-0">
+                                        <h3 class="card-title text-nowrap mb-2">Upcoming Appointments</h3>
+                                    </div>
+                                    <div class="d-flex align-items-center gap-2">
+                                        <div class="dropdown">
+                                            <button class="btn p-0" type="button" id="cardOpt4" data-bs-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">
+                                                <i class="bx bx-dots-vertical-rounded"></i>
+                                            </button>
+                                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt4">
+                                                <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- / Content -->
+@endsection
---
+++ routes/doctor_routes.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('backend.doctor.login');
+})->name('login');
+
+Route::get('/dashboard', function () {
+    return view('backend.doctor.dashboard');
+})->name('dashboard');
+
+Route::get('/appointments', function () {
+    return view('backend.doctor.appointments');
+})->name('appointments');
+
+Route::get('/schedule', function () {
+    return view('backend.doctor.schedule');
+})->name('schedule');
+
+Route::get('/patients', function () {
+    return view('backend.doctor.patients');
+})->name('patients');
+
+Route::get('/leave', function () {
+    return view('backend.doctor.leave');
+})->name('leave');
